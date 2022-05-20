97e6999f1492f70d18a2deab31fcfdb85d3d9de9
Carter S. Levinson <cslevo@posteo.net>
2022-05-20 14:18:25 -0400

    Add header file for test functions

---
+++ src/test.h
@@ -0,0 +1,45 @@
+/*!
+ * @file: test.h
+ * @brief: header file containing test functions for algs.h
+ * @author: Carter S. Levinson <cslevo@posteo.net>
+ */
+
+#ifndef TEST_H
+#define TEST_H
+
+void test_equal();
+
+void test_find();
+
+void test_rfind();
+
+void test_find_if();
+
+void test_search();
+
+void test_copy();
+
+void test_remove_copy();
+
+void test_remove_copy_if();
+
+void test_remove();
+
+void test_remove_if();
+
+void test_partition();
+
+void test_reverse();
+
+void test_accumulate();
+
+void test_for_each();
+
+void test_binary_search();
+
+void test_swap();
+
+void test_max();
+
+void test_min();
+#endif /* ifndef TEST_H */
