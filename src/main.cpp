211e8ad934900a6d6e5dc13ba787296204a13e5a
Carter S. Levinson <cslevo@posteo.net>
2022-05-20 15:19:56 -0400

    Added some output to main function for clarity

--- src/main.cpp
+++ src/main.cpp
@@ -8,26 +8,44 @@
 
 #include "test.h"
 
-int main(int argc, char *argv[]) {
-  std::cout << "Running the test suite for algs.h" << std::endl;
+int main() {
+  std::cout << "Running the test suite for algs.h..." << std::endl;
+  std::cout << "Testing the equal function..." << std::endl;
   test_equal();
+  std::cout << "Testing the find function..." << std::endl;
   test_find();
+  std::cout << "Testing the recursive find function..." << std::endl;
   test_rfind();
+  std::cout << "Testing the find if function..." << std::endl;
   test_find_if();
+  std::cout << "Testing the search function..." << std::endl;
   test_search();
+  std::cout << "Testing the copy function..." << std::endl;
   test_copy();
+  std::cout << "Testing the remove copy function..." << std::endl;
   test_remove_copy();
+  std::cout << "Testing the remove copy if function..." << std::endl;
   test_remove_copy_if();
+  std::cout << "Testing the remove function..." << std::endl;
   test_remove();
+  std::cout << "Testing the remove if function..." << std::endl;
   test_remove_if();
-  test_partition();
+  std::cout << "Testing the partition function..." << std::endl;
+  test_partition() ;
+  std::cout << "Testing the reverse function..." << std::endl;
   test_reverse();
+  std::cout << "Testing the accumulate function..." << std::endl;
   test_accumulate();
+  std::cout << "Testing the for each function..." << std::endl;
   test_for_each();
+  std::cout << "Testing the binary search function..." << std::endl;
   test_binary_search();
+  std::cout << "Testing the swap function..." << std::endl;
   test_swap();
+  std::cout << "Testing the max function..." << std::endl;
   test_max();
+  std::cout << "Testing the min function..." << std::endl;
   test_min();
-  std::cout << "All tests passed" << std::endl;
+  std::cout << "All tests passed! :)" << std::endl;
   return 0;
 }
