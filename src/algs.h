b66c34ec5b0501e8b5c53fa33d44a6057351f910
Carter S. Levinson <cslevo@posteo.net>
2022-05-20 16:19:05 -0400

    Moved implementation to header file because of how C++ templates work

--- src/algs.h
+++ src/algs.h
@@ -4,6 +4,21 @@
  * @author: Carter S. Levinson <cslevo@posteo.net>
  */
 
+/*
+ * Five iterator categories:
+ * 1. InputIt: sequential read-only access
+ * 2. OutputIt: sequential write-only access
+ * 3. ForwardIt: sequential read-write access
+ * 4. BidirectionalIt: reversible access
+ * 5. RandomIt: random access
+ *
+ * Other templated parameters:
+ * A. UnaryPredicate: singly quantified boolean function
+ * B. Accumulator: accumulator must stricly be a numeric type
+ * C. X, T: purely generic types
+ * D. Function: some generic function
+ */
+
 #ifndef ALGS_H
 #define ALGS_H
 
@@ -18,8 +33,12 @@ namespace algs {
  *
  * @returns true if the sequences match, false otherwise
  */
-template <class InputIt> bool equal(InputIt b1, InputIt e, InputIt b2);
-
+template <class InputIt>
+bool equal(InputIt b1, InputIt e, InputIt b2) {
+  while (b1 != e)
+    if (*b1++ != *b2++) return false;
+  return true;
+}
 /*!
  * Searches the sequence [b,e) for the element x
  *
@@ -30,7 +49,10 @@ template <class InputIt> bool equal(InputIt b1, InputIt e, InputIt b2);
  * @returns an inpu iter marking the element, iter equal to e if not found
  */
 template <class InputIt, class X>
-InputIt find(InputIt b, InputIt e, const X &x);
+InputIt find(InputIt b, InputIt e, const X& x) {
+  while (b != e && *b != x) ++b;
+  return b;
+}
 
 /*!
  * Recursively searches the sequence [b,e) for the element x
@@ -42,7 +64,11 @@ InputIt find(InputIt b, InputIt e, const X &x);
  * @returns an input iter marking the element, iter equal to e if not found
  */
 template <class InputIt, class X>
-InputIt rfind(InputIt b, InputIt e, const X &x);
+InputIt rfind(InputIt b, InputIt e, const X& x) {
+  if (b == e || *b == x) return b;
+  b++;
+  return rfind(b, e, x);
+}
 
 /*!
  * Searches the sequence [b,e) for the first element where predicate p is true
@@ -55,7 +81,10 @@ InputIt rfind(InputIt b, InputIt e, const X &x);
  * not found
  */
 template <class InputIt, class UnaryPred>
-InputIt find_if(InputIt b, InputIt e, UnaryPred p);
+InputIt find_if(InputIt b, InputIt e, UnaryPred p) {
+  while (b != e && !p(*b)) ++b;
+  return b;
+}
 
 /*!
  * Searches the sequence [b1,e1) for possibly different sized sequence [b2,e2)
@@ -69,7 +98,21 @@ InputIt find_if(InputIt b, InputIt e, UnaryPred p);
  * first, or e2 if not found
  */
 template <class ForwardIt>
-ForwardIt search(ForwardIt b1, ForwardIt e1, ForwardIt b2, ForwardIt e2);
+ForwardIt search(ForwardIt b1, ForwardIt e1, ForwardIt b2, ForwardIt e2) {
+  if (b2 == e2) return b1;
+  while (b1 != e1) {
+    ForwardIt it1 = b1;
+    ForwardIt it2 = b2;
+    while (*it1 == *it2) {
+      ++it1;
+      ++it2;
+      if (it2 == e2) return b1;
+      if (it1 == e1) return e1;
+    }
+    ++b1;
+  }
+  return e1;
+}
 
 /*!
  * Copies the entire sequence [b,e) to d
@@ -81,7 +124,10 @@ ForwardIt search(ForwardIt b1, ForwardIt e1, ForwardIt b2, ForwardIt e2);
  * @returns an output iter pointing to beginning of copied sequence
  */
 template <class InputIt, class OutputIt>
-OutputIt copy(InputIt b, InputIt e, OutputIt d);
+OutputIt copy(InputIt b, InputIt e, OutputIt d) {
+  while (b != e) *d++ = *b++;
+  return d;
+}
 
 /*!
  * Copies all elements in the sequence [b,e) not equal to x into the destination
@@ -95,7 +141,12 @@ OutputIt copy(InputIt b, InputIt e, OutputIt d);
  * @returns an output iter marking beginning of copied sequence
  */
 template <class InputIt, class OutputIt, class X>
-OutputIt remove_copy(InputIt b, InputIt e, OutputIt d, const X &x);
+OutputIt remove_copy(InputIt b, InputIt e, OutputIt d, const X& x) {
+  while (b != e) {
+    if (*b != x) *d++ = *b;
+    ++b;
+  }
+}
 
 /*!
  * Copies all elements x such that p(x) == false into the destination sequence
@@ -108,7 +159,12 @@ OutputIt remove_copy(InputIt b, InputIt e, OutputIt d, const X &x);
  * @return output iter marking the beginning of the copied sequence
  */
 template <class InputIt, class OutputIt, class UnaryPred>
-OutputIt remove_copy_if(InputIt b, InputIt e, OutputIt d, UnaryPred p);
+OutputIt remove_copy_if(InputIt b, InputIt e, OutputIt d, UnaryPred p) {
+  while (b != e) {
+    if (!p(b)) *d++ = *b;
+    ++b;
+  }
+}
 
 /*!
  * Alters sequece of range [b,e) into one with all values that compare equal to
@@ -123,7 +179,17 @@ OutputIt remove_copy_if(InputIt b, InputIt e, OutputIt d, UnaryPred p);
  * sequence
  */
 template <class ForwardIt, class X>
-ForwardIt remove(ForwardIt b, ForwardIt e, const X &x);
+ForwardIt remove(ForwardIt b, ForwardIt e, const X& x) {
+  ForwardIt ret = b;
+  while (b != e) {
+    if (!(*b == x)) {
+      if (ret != b) *ret = *b;
+      ++ret;
+    }
+    ++b;
+  }
+  return ret;
+}
 
 /*!
  * Alters the sequence of range [b,e) into one where all values where predicate
@@ -136,7 +202,17 @@ ForwardIt remove(ForwardIt b, ForwardIt e, const X &x);
  * @returns a forward iter marking the new of the range
  */
 template <class ForwardIt, class UnaryPred>
-ForwardIt remove_if(ForwardIt b, ForwardIt e, UnaryPred p);
+ForwardIt remove_if(ForwardIt b, ForwardIt e, UnaryPred p) {
+  ForwardIt ret = b;
+  while (b != e) {
+    if (!p(*b)) {
+      if (ret != b) *ret = *b;
+      ++ret;
+    }
+    ++b;
+  }
+  return ret;
+}
 
 /*!
  * Replaces all occurrences of x in the sequence [b,e) with y
@@ -147,7 +223,12 @@ ForwardIt remove_if(ForwardIt b, ForwardIt e, UnaryPred p);
  * @param y const reference to replacement value
  */
 template <class ForwardIt, class X>
-void replace(ForwardIt b, ForwardIt e, const X &x, const X &y);
+void replace(ForwardIt b, ForwardIt e, const X& x, const X& y) {
+  while (b != e) {
+    if (*b == x) *b = y;
+    ++b;
+  }
+}
 
 /*!
  * Transform sequence delimited by [b,e) such that all elements where prediated
@@ -160,7 +241,21 @@ void replace(ForwardIt b, ForwardIt e, const X &x, const X &y);
  * @returns a bidirectional iter pointing to first element of second partition
  */
 template <class BidirectionalIt, class UnaryPred>
-BidirectionalIt partition(BidirectionalIt b, BidirectionalIt e, UnaryPred p);
+BidirectionalIt partition(BidirectionalIt b, BidirectionalIt e, UnaryPred p) {
+  while (b != e) {
+    while (p(*b)) {
+      ++b;
+      if (b == e) return b;
+    }
+    do {
+      --e;
+      if (b == e) return b;
+    } while (!p(*e));
+    swap(*b, *e);
+    ++b;
+  }
+  return b;
+}
 
 /*!
  * Reverse the order of sequence [b,e)
@@ -169,7 +264,12 @@ BidirectionalIt partition(BidirectionalIt b, BidirectionalIt e, UnaryPred p);
  * @param e iter marking the end of the sequence
  */
 template <class BidirectionalIt>
-void reverse(BidirectionalIt b, BidirectionalIt e);
+void reverse(BidirectionalIt b, BidirectionalIt e) {
+  while (b != e) {
+    --e;
+    if (b != e) swap(*b++, *e);
+  }
+}
 
 /*!
  * Gathers the sum of sequence [b,e) into Accumulator a
@@ -182,7 +282,10 @@ void reverse(BidirectionalIt b, BidirectionalIt e);
  * sequence plus its initial value
  */
 template <class InputIt, class Accumulator>
-Accumulator accumulate(InputIt b, InputIt e, Accumulator a);
+Accumulator accumulate(InputIt b, InputIt e, Accumulator a) {
+  while (b != e) a += *b++;
+  return a;
+}
 
 /*!
  * Applies the function f to each element in the sequence [b,e)
@@ -194,7 +297,13 @@ Accumulator accumulate(InputIt b, InputIt e, Accumulator a);
  * @returns function f
  */
 template <class InputIt, class Function>
-Function for_each(InputIt b, InputIt e, Function f);
+Function for_each(InputIt b, InputIt e, Function f) {
+  while (b != e) {
+    f(*b);
+    ++b;
+  }
+  return f;
+}
 
 /*!
  * Performs simple binary search on sequence [b,e)
@@ -206,7 +315,19 @@ Function for_each(InputIt b, InputIt e, Function f);
  * @returns true if target value was found, otherwise false
  */
 template <class RandomIt, class X>
-bool binary_search(RandomIt b, RandomIt e, const X &x);
+bool binary_search(RandomIt b, RandomIt e, const X& x) {
+  while (b < e) {
+    // this calculation of mid avoids integer overflow
+    RandomIt mid = b + (e - b) / 2;
+    if (x < *mid)
+      e = mid;
+    else if (*mid < x)
+      b = mid + 1;
+    else
+      return true;
+  }
+}
+
 
 /*!
  * Swaps the values of two generic elements x and y
@@ -214,7 +335,12 @@ bool binary_search(RandomIt b, RandomIt e, const X &x);
  * @param x reference to first value
  * @param y reference to second value
  */
-template <class X> void swap(X &x, X &y);
+template <class X>
+void swap(X& x, X& y) {
+  X t = x;
+  x = y;
+  y = t;
+}
 
 /*!
  * Find the maximum of two generic elements
@@ -224,7 +350,10 @@ template <class X> void swap(X &x, X &y);
  *
  * @returns the larger of the two elements
  */
-template <class X> X max(const X &x, const X &y);
+template <class X>
+X max(const X& x, const X& y) {
+  return x > y ? x : y;
+}
 
 /*!
  * Find the minimum of two generic elements
@@ -234,6 +363,9 @@ template <class X> X max(const X &x, const X &y);
  *
  * @returns the smaller of the two elements
  */
-template <class X> X min(const X &x, const X &y);
+template <class X>
+X min(const X& x, const X& y) {
+  return x < y ? x : y;
+}
 } /* end namespace algs */
 #endif /* ifndef ALGS_H */
